ca5d0c6b3e441d9054853e955c8e38dc828ba900
pradyumn24 <78465660+pradyumn24@users.noreply.github.com>
2021-06-23 18:05:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-console.log("My first console project"); 
-var first_name = "Ranbir";
- var last_name = "Kapoor"; var full_name = first_name.concat(last_name); console.log(full_name); var a = 10; var b = 5; var add = a + b ; console.log(add); var sub = a - b ; console.log(sub); var multiple = a * b ; console.log(multiple); 
-var divide = a / b ; console.log(divide);
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg",
+"https://i.postimg.cc/L6HcYQM0/grand-father.png",
+ "https://i.postimg.cc/wjMnFtMX/father.jpg" ,
+  "https://i.postimg.cc/5ymDKL83/bro.jpg",
+   "https://i.postimg.cc/JnL6wtrd/sister.jpg", 
+   "https://i.postimg.cc/bw5W5zSK/mother.jpg"]; 
+   var names = ["Fmaily Book",
+"Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"]; 
+var i = 0;
+ function update() { i++; var numbers_of_family_member_in_array = 5
+     if(i > numbers_of_family_member_in_array ) { i = 0; } var updatedImage = images[i]; var updatedName = names[i]; document.getElementById("family_member_image").src = updatedImage; document.getElementById("family_member_name").innerHTML = updatedName; }
